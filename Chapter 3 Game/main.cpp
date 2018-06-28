0f99664086938bf2c01f4e05de4940a5ea0d9d88
Sangha Yoon <sanghayoon@sanghas-macbook-air.local>
2018-06-27 21:59:48 -0700

    Initial Commit

---
+++ Chapter 3 Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter 3 Game
+//
+//  Created by Sangha Yoon on 6/27/18.
+//  Copyright © 2018 Sangha Yoon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
